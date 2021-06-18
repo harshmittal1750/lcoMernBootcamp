ae974d2037437e0eeb1b2914c05563d7cacce175
harshmittal <harshmittal1750@gmail.com>
2021-06-18 17:33:59 +0530

    userScheme

--- projbackend/models/user.js
+++ projbackend/models/user.js
@@ -13,4 +13,28 @@ var userSchema = new moongoose.Schema({
     maxLength: 32,
     trim: true,
   },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+  },
+  userinfo: {
+    type: String,
+    trim: true,
+  },
+  // TODO: come back here and
+  password: {
+    type: String,
+    role: {
+      type: Number,
+      default: 0,
+    },
+    purchases: {
+      type: Array,
+      default: [],
+    },
+  },
 });
+
+module.exports = mongoose.model("User", userSchema);
