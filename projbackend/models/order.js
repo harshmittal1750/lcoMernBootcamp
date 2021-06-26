5ab2583e333aa4cdfd4d845f1113559846f2aab1
harshmittal <harshmittal1750@gmail.com>
2021-06-26 06:51:18 +0530

    order.js

---
+++ projbackend/models/order.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+const ProductCartSchema = new mongoose.Schema({
+  product: {
+    type: ObjectId,
+    ref: "Product",
+  },
+  name: String,
+  count: Number,
+  price: Number,
+});
+const ProductCart = mongoose.model("ProductCart", ProductCartSchema);
+const OrderSchema = new mongoose.Schema(
+  {
+    poducts: [ProductCart.Schema],
+    transaction_id: {},
+    amount: { type: Number },
+    address: String,
+    updated: Date,
+    user: {
+      type: ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+const Order = mongoose.model("Order", OrderSchema);
+module.exports = mongoose.model("Order", ProductCart);
