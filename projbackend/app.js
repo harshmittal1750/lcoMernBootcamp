0075feba260866dd6fc5c9083063f5fca99804cb
harsh mittal <harshmittal1750@gmail.com>
2021-07-01 11:29:09 +0530

    env file for uploading on server

--- projbackend/app.js
+++ projbackend/app.js
@@ -1,18 +1,19 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 
 mongoose
-  .connect("mongodb://localhost:27017/tshirt", {
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
+    useCreateIndex: true,
   })
   .then(() => {
     console.log("DB CONNECTED");
   });
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`app is running at ${port}`);
