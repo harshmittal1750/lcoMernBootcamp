d286c5bdf64749494e0982f26d2f8c4e34f67cc4
harsh mittal <harshmittal1750@gmail.com>
2021-07-05 02:55:52 +0530

    testback-middleware

--- testbackend/index.js
+++ testbackend/index.js
@@ -1,15 +1,42 @@
 const express = require("express");
+
 const app = express();
+
 const port = 8000;
-app.get("/homepage", (req, res) => {
-  return res.send("Hello there");
+
+app.get("/", (req, res) => {
+  return res.send("Home page");
 });
+
+const admin = (req, res) => {
+  return res.send("this is admin dashboard");
+};
+
+const isAdmin = (req, res, next) => {
+  console.log("isAdmin is running");
+  next();
+};
+const isLoggedin = (req, res, next) => {
+  console.log("isLogged in successfully");
+  next();
+};
+
+app.get("/admin", isLoggedin, isAdmin, admin);
+
 app.get("/login", (req, res) => {
-  return res.send("Hello user, u r sign in");
+  return res.send("You are visiting login route");
 });
-app.get("/signout", (req, res) => {
-  return res.send("Hello user, u r sign out");
+
+app.get("/signup", (req, res) => {
+  return res.send("You are visiting signup route");
 });
+
 app.listen(port, () => {
-  console.log("Server is fine");
+  console.log("Server is up and running...");
 });
+
+// const port = 3000
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
