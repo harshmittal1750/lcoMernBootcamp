3fd1d1fa0fe6b26174eb81dbc01c33c19996d43e
Harsh Mittal <harshmittal1750@gmail.com>
2021-10-19 19:23:34 +0530

    till 08 with error

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,5 +1,17 @@
+const User = require("../models/user");
+exports.signup = (req, res) => {
+  const user = new User(req, body);
+  user.save((err, user) => {
+    if (err) {
+      return res.status(400).send({
+        err: "NOT able to save user in DB",
+      });
+      res.json("user");
+    }
+  });
+};
 exports.signout = (req, res) => {
   res.json({
-    message: "User signout"
+    message: "User signout",
   });
 };
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
-const { signout } = require("../controllers/auth");
-
+const { signout, signup } = require("../controllers/auth");
+router.post("/signup", signup);
 router.get("/signout", signout);
 
 module.exports = router;
